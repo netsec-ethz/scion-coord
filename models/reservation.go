d3402f4c25a11c29ec45292b08e49a5365753fd8
Kilian Gärtner <Bhaals@gmx.de>
2017-07-18 12:56:05 +0200

    Check credit saldo before reservation
    Deny a reservation if the AS has not anymore credits.

--- models/reservation.go
+++ models/reservation.go
@@ -26,10 +26,40 @@ func FindReservationsBySource(source addr.ISD_AS) ([]Reservation, error) {
 	return reservations, err
 }
 
-func (reservation *Reservation) Insert() error {
+func FindTodaysReservationsBySource(source addr.ISD_AS) ([]Reservation, error) {
 
-	// TODO: Check, if the as_source has enough credits to make a reservation
+	var now,today,tommorow
+	now := time.Now()
+	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
+	tommorow := time.AddDate(0,0,1)
+	var reservations []Reservation
+	standardDuration := 16 * time.Second
+	_, err := o.QueryTable("reservation",
+	).Filter("SourceIsd", source.I).Filter("SourceAs", source.A, // Filter all reservation BY an ISD-AS
+      	).Filter("time__gte", today).Filter("time__lte", tommorow, // Filter all reservation which are valid (-16 seconds from NOW())
+	).All(&reservations)
+	return reservations, err
+}
+
+func (reservation *Reservation) Insert() error {
+	var reservations []Reservation
+	// TODO: Make the interval variable
+	reservations, err := FindTodaysReservationsBySource(addr.ISD_AS(reservation.SourceIsd,reservation.SourceAs))
+	if err != nill
+		return err
+	var creditsToday = reservation.Amount / 10
+	for i, v := range reservations {
+		creditsToday += v.Amount / 10
+	}
+	
+	
+	as, err := models.FindAsByIsdAs(reservation.SourceIsd + "-"+reservation.SourceAs)
+	if as.Credits <= creditsToday {
+		err := errors.New("no more credits for today")
+		return err
+	}
 	_, err := o.Insert(reservation)
+	
 	return err
 }
 
