4894b9bf57dc8546cd65ac7f1a06878d9d566c2d
chaehni <chaehni@student.ethz.ch>
2017-09-01 10:26:27 +0200

    Captcha for user registration

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,18 @@
 'use strict';
 
 angular.module('scionApp', [
-  'ngRoute'
+  'ngRoute', 'vcRecaptcha'
 
 ]).config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
       .when('/register', {
         templateUrl: '/public/partials/register.html',
-        controller: 'registerCtrl'
+        controller: 'registerCtrl',
+        resolve: {
+            ResolveSiteKey: ['registerService', function(registerService){
+            return registerService.getSiteKey()
+          }]
+        }
       })
       .when('/login', {
         templateUrl: '/public/partials/login.html',
--- public/js/controllers/register.js
+++ public/js/controllers/register.js
@@ -1,25 +1,31 @@
 angular.module('scionApp')
-    .controller('registerCtrl', ['$scope', 'registerService', '$interval', '$location',
-        function($scope, registerService, $interval, $location) {
+    .controller('registerCtrl', ['$scope', 'ResolveSiteKey', '$interval', '$location', 'vcRecaptchaService',
+        function($scope, ResolveSiteKey, $interval, $location, vcRecaptchaService) {
 
-            $scope.error = "";
-            $scope.message = "";
             $scope.user = {};
+            $scope.siteKey = ResolveSiteKey.data;
 
-            // refresh the list of processes
             $scope.register = function(user) {
+
+                if (!$scope.user.captcha){
+                    $scope.error = "Please resolve the captcha before submitting."
+                    $scope.message = "";
+                } else {
 
                 registerService.register(user).then(
                     function(data) {
                         $scope.message = "Registration completed successfully. We sent you an email to your inbox with a link to verify your account.";
                         $scope.error = ""
                         $scope.user = {};
+                        vcRecaptchaService.reload();
                     },
                     function(response) {
                         $scope.error = response.data;
                         $scope.message = ""
+                        vcRecaptchaService.reload();
                         console.log(response);
                     });
+                }
             };
 
         }
