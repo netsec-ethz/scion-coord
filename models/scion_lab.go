b726cd4a628b0a1b92eb931e38480c7fe43799be
jonghoonkwon <jonghoonkwon@gmail.com>
2018-02-14 21:48:32 +0100

    Bugfix: wrongly updated joinBRID

--- models/scion_lab.go
+++ models/scion_lab.go
@@ -413,7 +413,6 @@ func (as *SCIONLabAS) UpdateDBConnection(cnInfo *ConnectionInfo) error {
 		}
 		cn.JoinStatus = cnInfo.Status
 		cn.RespondStatus = cnInfo.NeighborStatus
-		cn.JoinBRID = cnInfo.BRID
 	}
 	if respondAS.ID == as.ID {
 		if !cn.IsVPN {
