5d6878d5e622ec9f9e2399dfd942ebd98e37b2ec
Markus Legner <mlegner@users.noreply.github.com>
2018-02-19 13:09:35 +0100

    Check owner of Attachment Point before giving access to APIs (#160)

--- controllers/api/scion_lab_ap.go
+++ controllers/api/scion_lab_ap.go
@@ -99,7 +99,6 @@ func (s *SCIONLabASController) GetUpdatesForAP(w http.ResponseWriter, r *http.Re
 	apIA, err := s.checkAuthorization(r)
 	if err != nil {
 		s.Forbidden(w, err, "The account is not authorized for this AP")
-		return
 	}
 
 	cns, err := models.FindRespondConnectionInfoByIA(apIA)
