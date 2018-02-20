024a12b8da3b9b2d7e9a1da3fb846ca3691b354f
Milan Pandurov <milanpandurov@protonmail.com>
2018-02-20 18:01:25 +0100

    Added dummy update script to download

--- scionupgrade.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#TODO: Download newest upgrade script and run it
-
-run_time=$(date)
-echo "${run_time}" >> /tmp/scionupgrade.log
---
+++ vagrant/scionupgrade.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#TODO: Load client ID and client secret
+
+wget https://gist.githubusercontent.com/xabarass/def1f861f0fbb1f51d7479d23e239c6f/raw/1de4daf4594abd28d383a0a0d1d0110e9398c670/upgrade.sh
+chmod +x upgrade.sh
+
+./upgrade.sh
+
+OUT=$?
+if [ $OUT -eq 0 ];then
+   echo "Update script executed without errors."
+   #TODO: Report success
+else
+   echo "Update script exited with error code!"
+   #TODO: Report error
+fi
