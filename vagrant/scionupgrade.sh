51a8b2afedf9360622af388c649da7186cfbc170
Milan Pandurov <milanpandurov@protonmail.com>
2018-02-20 18:03:37 +0100

    Added placeholder for scion update script

--- vagrant/scionupgrade.sh
+++ vagrant/scionupgrade.sh
@@ -2,6 +2,7 @@
 
 #TODO: Load client ID and client secret
 
+#TODO: replace URL with correct address
 wget https://gist.githubusercontent.com/xabarass/def1f861f0fbb1f51d7479d23e239c6f/raw/1de4daf4594abd28d383a0a0d1d0110e9398c670/upgrade.sh
 chmod +x upgrade.sh
 
