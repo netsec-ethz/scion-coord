7b45b3d062e371c7ef3f96ea333ad88f2eaa4e74
Milan Pandurov <milanpandurov@protonmail.com>
2018-02-21 20:26:49 +0100

    Upgrade script is reading account id and account secret

--- vagrant/scionupgrade.sh
+++ vagrant/scionupgrade.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-#TODO: Load client ID and client secret
+ACCOUNT_ID=$(<"$SC/gen/account_id")
+ACCOUNT_SECRET=$(<"$SC/gen/account_secret")
+IA=$(<"$SC/gen/ia")
 
 #TODO: replace URL with correct address
-wget https://gist.githubusercontent.com/xabarass/def1f861f0fbb1f51d7479d23e239c6f/raw/1de4daf4594abd28d383a0a0d1d0110e9398c670/upgrade.sh
+wget https://gist.githubusercontent.com/xabarass/def1f861f0fbb1f51d7479d23e239c6f/raw/0d3220b63d82b54df87ed412c5a4db876d2c438c/upgrade.sh
 chmod +x upgrade.sh
 
-./upgrade.sh
+./upgrade.sh $ACCOUNT_ID $ACCOUNT_SECRET $IA
 
-OUT=$?
-if [ $OUT -eq 0 ];then
-   echo "Update script executed without errors."
-   #TODO: Report success
-else
-   echo "Update script exited with error code!"
-   #TODO: Report error
-fi
