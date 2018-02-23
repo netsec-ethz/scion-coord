4a0e94b7031a5610c2be5015bf687ebb93fa0c6b
mlegner <markus@legner.ch>
2018-02-23 10:13:32 +0100

    Inserted correct link of `scion_upgrade_script.sh`

--- vagrant/scionupgrade.sh
+++ vagrant/scionupgrade.sh
@@ -4,8 +4,7 @@ ACCOUNT_ID=$(<"$SC/gen/account_id")
 ACCOUNT_SECRET=$(<"$SC/gen/account_secret")
 IA=$(<"$SC/gen/ia")
 
-#TODO: replace URL with correct address
-wget https://gist.githubusercontent.com/xabarass/def1f861f0fbb1f51d7479d23e239c6f/raw/0d3220b63d82b54df87ed412c5a4db876d2c438c/upgrade.sh
+wget https://raw.githubusercontent.com/netsec-ethz/scion-coord/master/scion_upgrade_script.sh
 chmod +x upgrade.sh
 
 ./upgrade.sh $ACCOUNT_ID $ACCOUNT_SECRET $IA
