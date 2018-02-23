ab6f3c122b1399640cc5650166d8bca498b013c9
Juan A. Garcia Pardo <juagargi@gmail.com>
2018-02-23 14:26:57 +0100

    Install script. Detect username.

---
+++ scripts/install-scion-coord.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+basedir="$(realpath $(dirname $(realpath $0))/../)"
+
+# check binary
+if [ ! -x "$basedir/scion-coord" ]; then
+    echo "SCION Coordinator binary file scion-coord is missing. Building."
+    pushd "$basedir" >/dev/null
+    go build
+    if [ -x "scion-coord" ]; then
+        echo "Built."
+    else
+        echo "Still didn't find the binary. Abort."
+        exit 1
+    fi
+    popd >/dev/null
+fi
+
+# TODO: in the future we probably want to perform checks against the DB, the credentials, 
+# the scionLabConfigs/easy-rsa and basically anything that we perform manually
+
+# check service file
+if [ ! -f "$basedir/scripts/scion-coord.service" ]; then
+    echo "Missing service file"
+fi
+if [ ! -d "/etc/systemd/system" ]; then
+    echo "Cannot find the destination directory for the service file. /etc/systemd/system not found. Abort."
+    exit 1
+fi
+
+sudo systemctl stop "scion-coord" || true
+pushd "/etc/systemd/system" >/dev/null
+if [ -f "scion-coord.service" ]; then
+    echo "There exist a previous scion-coord.service file. Moving it to /tmp as a backup measure."
+    sudo mv "scion-coord.service" "/tmp/scion-coord.service.bak.from-scion-coord-installer"
+fi
+tmpfile=$(mktemp)
+cp "$basedir/scripts/scion-coord.service" "$tmpfile"
+sed -i -- "s/_USER_/$USER/g" "$tmpfile"
+sudo cp "$tmpfile" "scion-coord.service"
+popd >/dev/null
+
+sudo systemctl daemon-reload
+sudo systemctl start "scion-coord"
+sleep 1
+systemctl status "scion-coord" >/dev/null && fail=0 || fail=1
+if [ $fail -ne 0 ]; then
+    echo "Problem starting the service. Please check with systemctl status scion-coord or journalctl -xe"
+    exit 1
+fi
+sudo systemctl enable "scion-coord"
+echo "Success."
