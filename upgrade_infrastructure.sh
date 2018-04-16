8f1932dbc15042fe7d2de67668964509091f6af3
Juan A. Garcia Pardo <juagargi@gmail.com>
2018-04-16 12:07:59 +0200

    Changes from review.
    Fix repo and branch names. Allow branch as parameter.
    Don't use sudo with password.
    Check git status.
    Backup locally the current branch.
    Zookeeper clean slate instead of supervisor reload.

--- upgrade_infrastructure.sh
+++ upgrade_infrastructure.sh
@@ -2,11 +2,40 @@
 
 set -e
 
-GITREPO="juan"
-GITBRANCH="stable_scionproto"
-MAKECHANGES=1
+GITREPO="origin"
+GITBRANCH="scionlab"
+BACKUPBRANCH="backup_upgrade1"
+MAKECHANGES=0
 # This script should be run on each of the ASes that are part of the infrastructure.
 # Ideally called with ansible or forallASes -f
+
+
+usage="$(basename $0) [-b branchname]
+
+where:
+    -b branchname:  Upgrade to branchname. Default value is scionlab"
+while getopts ":b:h" opt; do
+case $opt in
+    h)
+        echo "$usage"
+        exit 0
+        ;;
+    b)
+        GITBRANCH="$OPTARG"
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        echo "$usage" >&2
+        exit 1
+        ;;
+    :)
+        echo "Option -$OPTARG requires an argument." >&2
+        echo "$usage" >&2
+        exit 1
+        ;;
+esac
+done
+echo "Using branch $GITBRANCH"
 
 # check we have $SC, and gen folder
 if [ ! -d "$SC" ]; then
@@ -19,8 +48,8 @@ if [ ! -d "gen" ]; then
     exit 1
 fi
 
-# check we can sudo. We have -e, will exit if not
-echo sci0nLab | sudo -S -v &>/dev/null && success=1 || success=0
+# check we can sudo
+sudo -S -v < /dev/null && success=1 || success=0
 if [ $success != 1 ]; then
     echo "sudo failed"
     exit 1
@@ -28,7 +57,19 @@ fi
 
 currentref=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
 if [ "$currentref" != "${GITREPO}/${GITBRANCH}" ]; then 
-    echo "Not currently using reference $GITBRANCH, but $currentref"
+    echo "Not currently using reference ${GITREPO}/${GITBRANCH}, but $currentref"
+    exit 1
+fi
+
+if git branch | grep "$BACKUPBRANCH"; then
+    echo "Branch $BACKUPBRANCH exists already. Aborting."
+    exit 1
+fi
+
+if [ $(git status --untracked-files=no --short | wc -l) != 0 ]; then
+    echo "Local copy of repository modified:"
+    git status --untracked-files=no
+    echo "Aborting."
     exit 1
 fi
 
@@ -44,6 +85,7 @@ if [ "$MAKECHANGES" -ne 1 ]; then
 fi
 echo "All checks OK."
 ########################################################## MODIFYING THE AS HERE ###############
+git branch "$BACKUPBRANCH"
 output=$(./scion.sh stop 2>&1) && success=1 || success=0
 if [ $success != 1 ]; then
     echo "SCION stop failed:"
@@ -71,9 +113,9 @@ if [ $success != 1 ]; then
     echo "$output"
     # exit 1
 fi
-output=$(./supervisor/supervisor.sh reload 2>&1) && success=1 || success=0
+output=$(./tools/zkcleanslate --zk 127.0.0.1:2181 2>&1) && success=1 || success=0
 if [ $success != 1 ]; then
-    echo "SCION supervisor reload failed:"
+    echo "Zookeeper cleanslate failed:"
     echo "$output"
     # exit 1
 fi
