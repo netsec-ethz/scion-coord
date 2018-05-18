3ffc27b99b6b5123411d0bfde5c32543ea4142af
Juan A. Garcia Pardo <juagargi@gmail.com>
2018-05-18 15:57:56 +0200

    Dockerize things.
    First steps to have an automated testing environment using docker.
    It's still not fully developed, but it works and it's self contained.

--- docker.sh
+++ docker.sh
@@ -1,3 +1,64 @@
-#~/bin/bash
-# docker build -t juagargi/scionlab-scion:0.1 -f docker/Dockerfile-scion .
-docker build -t juagargi/scionlab-coord:0.1 -f docker/Dockerfile-coord .
+#!/bin/bash
+
+VERSION=0.1
+
+BASE="$(dirname $(realpath $0))"
+
+build_scion_image() {
+    pushd $SC >/dev/null
+    # need_base=0
+    # [[ $(docker images -q scion:scionlab) != "" ]] && need_scion=0 || { need_scion=1 && [[ $(docker images -q scion_base:scionlab) != "" ]] || need_base=1 ; } 
+    # echo $need_base
+    # echo $need_scion
+    # if [ $need_base == 1 ]; then
+    #     ./docker.sh base
+    # fi
+    # if [ $need_scion == 1 ]; then
+    #     ./docker.sh build
+    # fi
+    ./docker.sh base
+    ./docker.sh build
+    popd >/dev/null
+}
+
+build() {
+    build_scion_image
+
+    # # docker build -t juagargi/scionlab-scion:0.1 -f docker/Dockerfile-scion .
+    docker build -t scionlab-coord:0.1 -f docker/Dockerfile-coord .
+    docker build -t scionlab-coord-test -f docker/Dockerfile-coord-test .
+    # docker push juagargi/scionlab-coord:0.1
+}
+
+rebuild() {
+    # for now, just remove the image and build
+    docker rmi scionlab-coord:0.1 scionlab-coord-test || true
+    build
+}
+
+test() {
+    # asfdasd
+    cd $BASE
+    docker-compose -f docker/test-coordinator.yml up --abort-on-container-exit --exit-code-from test
+    TEST1=$?
+    echo "Test1 exit status: $TEST1"
+}
+
+
+
+
+usage="$(basename $0) {build|rebuild|test}
+
+where:
+    build           builds the containers
+    rebuild         force-builds the Coordinator and test containers
+    test            runs the tests"
+
+case "$1" in
+    build)          build ;;
+    rebuild)        rebuild ;;
+    test)           test ;;
+    *)              echo "$usage";;
+esac
+
+
---
+++ docker/testscripts/test1.sh
@@ -0,0 +1,366 @@
+#!/bin/bash
+
+# Configure a coordinator service and test SCION works with it
+
+set -e
+
+# check and export paths:
+if [ -z "$GOPATH" ]; then
+    GOPATH="$HOME/go"
+fi
+
+MYSQLCMD="mysql -u root -pdevelopment_pass -h mysql"
+NETSEC=${GOPATH:?}/src/github.com/netsec-ethz
+SCIONCOORD="$NETSEC/scion-coord"
+# SCION="${GOPATH:?}/src/github.com/scionproto/scion"
+SCIONBOXLOCATION="$SCIONCOORD/sub/scion-box"
+CONFDIR="$HOME/scionLabConfigs"
+EASYRSADEFAULT="$SCIONCOORD/conf/easy-rsa_vars.default"
+TESTTIMEOUT=8
+
+ACC_ID="someid"
+ACC_PW="some_secret"
+INTF_ADDR="127.0.0.5"
+SCION_COORD_URL="http://localhost:8080"
+
+missingOrDifferentFiles() {
+    ! [[ -f "$1" ]] || ! [[ -f "$2" ]] || ! cmp "$1" "$2" >/dev/null
+}
+
+runSQL() {
+    SQL=$1
+    output=$(echo $SQL | $MYSQLCMD 2>&1) ; FAIL=$?
+    output=$(echo "$output" | grep -Fv [Warning])
+    echo "$output"
+    return $FAIL
+}
+
+onExit() {
+    RET=$?
+    trap '' INT TERM
+    # if [ "$doTest" -eq 1 ]; then
+    #     # this means that we have stopped and started the scion ourselves. Stop it back
+    #     cd "$SC"
+    #     "./scion.sh" stop || true
+    # fi
+    if [ ! -z $scionCoordPid ]; then
+        # maybe kill SCION Coord if it's running and wait
+        kill -TERM 0
+        wait
+    fi
+    if [ ! -z "$restoreGen" ]; then
+        rm -rf "$SC/gen-previous-coordinator-test-run"
+        mv "$SC/gen" "$SC/gen-previous-coordinator-test-run"
+        mv "$restoreGen" "$SC/gen"
+    fi
+    [[ ! -z "$exitMessage" ]] && printf "$exitMessage""\n"
+    exit $RET
+}
+trap onExit EXIT INT TERM
+
+cleanZookeeper() {
+    if [ -x /usr/share/zookeeper/bin/zkCli.sh ] && [ /usr/share/zookeeper/bin/zkServer.sh status &>/dev/null ]; then
+        printf 'rmr /1-11 \nrmr /1-12 \nrmr /1-13 \nrmr /1-1001 \n' | /usr/share/zookeeper/bin/zkCli.sh &>/dev/null
+    fi
+}
+
+archiveGenFolder() {
+    if [ -d "$SC/gen" ]; then
+        restoreGen=$(mktemp -d)
+        rmdir "$restoreGen"
+        mv "$SC/gen" "$restoreGen"
+    fi
+}
+
+scionCoordPid=''
+exitMessage=''
+restoreGen=''
+CURRENTWD="$PWD"
+basedir="$(realpath $(dirname $(realpath $0))/../)"
+mkdir -p "$NETSEC"
+cd "$NETSEC"
+
+doTest=1
+# usage="$(basename $0) [-n]
+
+# where:
+#     -n      No test: only set up SCION + coordinator, run coordinator and wait for it to finish"
+# while getopts ":n" opt; do
+# case $opt in
+#     h)
+#         echo "$usage"
+#         exit 0
+#         ;;
+#     n)
+#         doTest=0
+#         ;;
+#     \?)
+#         echo "Invalid option: -$OPTARG" >&2
+#         echo "$usage" >&2
+#         exit 1
+#         ;;
+#     :)
+#         echo "Option -$OPTARG requires an argument." >&2
+#         echo "$usage" >&2
+#         exit 1
+#         ;;
+# esac
+# done
+
+
+
+# # Custom configuration
+# if [ ! -f "$SCIONCOORD/conf/development.conf" ]; then
+#     # copy the default configuration and edit it for postmark
+#     cp "$SCIONCOORD/conf/development.conf.default" "$SCIONCOORD/conf/development.conf"
+#     sed -i -- 's/email.pm_server_token = ""/email.pm_server_token = "server_token"/g' "$SCIONCOORD/conf/development.conf"
+#     sed -i -- 's/email.pm_account_token = ""/email.pm_account_token = "account_token"/g' "$SCIONCOORD/conf/development.conf"
+# fi
+
+# MySQL DB step. If already installed, we assume it's already configured the way we want
+# if ! dpkg-query -s mysql-server &> /dev/null ; then
+#     echo "mysql-server-5.7 mysql-server/root_password password development_pass" | sudo debconf-set-selections
+#     echo "mysql-server-5.7 mysql-server/root_password_again password development_pass" | sudo debconf-set-selections
+#     DEBIAN_FRONTEND="noninteractive" sudo apt-get install mysql-server -y
+# fi
+# # check if mysqld is running:
+# if ! pgrep -x "mysqld" &>/dev/null; then
+#     echo "MySQL is not running. Starting the service."
+#     sudo systemctl restart mysql
+# fi
+
+# if ! dpkg-query -s easy-rsa &> /dev/null ; then
+#     DEBIAN_FRONTEND="noninteractive" sudo apt-get install easy-rsa -y
+# fi
+
+
+# while true; do
+#     echo heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+#     date
+#     sleep 1
+# done
+
+
+while ! mysqladmin ping -h mysql --silent; do
+    sleep 1
+done
+runSQL "DROP DATABASE scion_coord_test;" &>/dev/null || true
+runSQL "CREATE DATABASE scion_coord_test;" || (echo "Failed to create the SCION Coordinator DB" && exit 1)
+
+# if ! runSQL "SHOW DATABASES;" | grep "scion_coord_test" &> /dev/null; then
+#     runSQL "CREATE DATABASE scion_coord_test;" || (echo "Failed to create the SCION Coordinator DB" && exit 1)
+# else
+#     echo "Removing entries in the DB"
+#     # we need the id > 0 to convince mysql that it is secure
+#     runSQL "DELETE FROM scion_coord_test.connection WHERE id > 0 AND respond_ap IN (
+#     SELECT id FROM scion_coord_test.attachment_point WHERE as_ID IN (
+#         SELECT id from scion_coord_test.scion_lab_as WHERE user_email='netsec.test.email@gmail.com'
+#         )
+#     );" &>/dev/null || true
+#     runSQL "DELETE FROM scion_coord_test.attachment_point WHERE id > 0 AND as_id IN (
+#     SELECT id FROM scion_coord_test.scion_lab_as WHERE user_email='netsec.test.email@gmail.com'
+#     );" &>/dev/null || true
+#     runSQL "DELETE FROM scion_coord_test.scion_lab_as WHERE id > 0 AND user_email='netsec.test.email@gmail.com';" &>/dev/null || true
+#     runSQL "DELETE FROM scion_coord_test.user WHERE id > 0 AND email='netsec.test.email@gmail.com';" &>/dev/null || true
+#     runSQL "DELETE FROM scion_coord_test.account WHERE id > 0 AND name='netsec.test.email@gmail.com';" &>/dev/null || true
+# fi
+
+# # now copy the three credentials files from the SCION installation to the coordinator
+# # we should do this per ISD with an attachment point
+# mkdir -p "$SCIONCOORD/credentials"
+# cd "$SCIONCOORD/credentials"
+# VERS=$(ls $SC/gen/ISD1/AS11/br1-11-1/certs/*.crt | sort -r | awk 'NR==1{print $1}' | sed -n 's/^.*ISD1-AS11-V\([0-9]*\)\.crt/\1/p')
+# if missingOrDifferentFiles "$SC/gen/ISD1/AS11/br1-11-1/keys/as-sig.seed" ISD1.key ||
+#    missingOrDifferentFiles "$SC/gen/ISD1/AS11/br1-11-1/certs/ISD1-AS11-V$VERS.crt" ISD1.crt ||
+#    missingOrDifferentFiles "$SC/gen/ISD1/AS11/br1-11-1/certs/ISD1-V$VERS.trc" ISD1.trc ;
+# then
+#     echo "Credentials in SCION Coord. Serv. seem different. Running SCION and using those"
+#     pushd "$SC" >/dev/null
+#     ./scion.sh topology -c topology/Tiny.topo
+#     popd >/dev/null
+
+#     cp "$SC/gen/ISD1/AS11/br1-11-1/keys/as-sig.seed" ISD1.key
+#     VERS=$(ls $SC/gen/ISD1/AS11/br1-11-1/certs/*.crt | sort -r | awk 'NR==1{print $1}' | sed -n 's/^.*ISD1-AS11-V\([0-9]*\)\.crt/\1/p')
+#     cp "$SC/gen/ISD1/AS11/br1-11-1/certs/ISD1-AS11-V$VERS.crt" ISD1.crt
+#     cp "$SC/gen/ISD1/AS11/br1-11-1/certs/ISD1-V$VERS.trc" ISD1.trc
+# fi
+
+# # VPN stuff
+# if [ ! -f "$CONFDIR/easy-rsa/keys/ca.crt" ]; then
+#     # generate certificate for openVPN
+#     if ! dpkg-query -s easy-rsa &> /dev/null ; then
+#         DEBIAN_FRONTEND="noninteractive" sudo apt-get install easy-rsa -y
+#     fi
+#     if ! dpkg-query -s openssl &> /dev/null ; then
+#         DEBIAN_FRONTEND="noninteractive" sudo apt-get install openssl -y
+#     fi
+#     mkdir -p "$CONFDIR"
+#     cp -r /usr/share/easy-rsa "$CONFDIR"
+#     cp "$EASYRSADEFAULT" "$CONFDIR/easy-rsa/vars"
+#     pushd "$CONFDIR/easy-rsa" >/dev/null
+#     sed -i -- 's/export KEY_EMAIL="scion@lists.inf.ethz.ch"/export KEY_EMAIL="netsec.test.email@gmail.com"/g' ./vars
+#     source ./vars
+#     ./clean-all
+#     # build the CA non interactively
+#     ./pkitool --initca
+#     popd >/dev/null
+# fi
+
+
+
+# create the initial tables:
+cd "$SCIONCOORD"
+rm -f scion-coord
+go build
+./scion-coord --help >/dev/null
+
+
+# populate the test DB accordingly. For now with one attachment point, in ISD1 AS12
+sql="SELECT COUNT(*) FROM scion_coord_test.account WHERE name='netsec.test.email@gmail.com';"
+out=$(runSQL "$sql") && stat=0 || stat=$?
+out=$(echo "$out" | tail -n 1)
+if [ $out -ne 0 ]; then
+    exitMessage="Inconsistent result: we deleted all data related to the test in the DB, but still have an account. Aborting."
+    exit 1
+fi
+
+sql="INSERT INTO scion_coord_test.account
+(id, name, organisation, account_id, secret, created, updated)
+VALUES
+(1, 'netsec.test.email@gmail.com', 'NETSEC TEST', '$ACC_ID', '$ACC_PW', NOW(), NOW())
+"
+out=$(runSQL "$sql") && stat=0 || stat=$?
+
+# password is "scionscion"
+sql="INSERT INTO scion_coord_test.user
+(id, email, password, password_invalid, salt,
+first_name, last_name, verified, is_admin, verification_uuid, account_id, created, updated)
+VALUES
+(1, 'netsec.test.email@gmail.com', '81c0cd129972d7f5ebda612da8c13528e80068705330170121d9b07bdc52b7f0', 0, '286301951c5da8c82dd34f6123ce05ef17fc0f0c1032067eca4a909c0f0f03e85c0123f3c8510afec0809aebfb74dafad300c4a847c787e34628a2bb5c336e94705ab076c9103452064ce448be2a416c',
+'first name', 'last name', 1, 0, '0371c50c-511d-417f-bbee-949df9fe52c6',
+1, NOW(), NOW()
+)"
+out=$(runSQL "$sql") && stat=0 || stat=$?
+
+sql="INSERT INTO scion_coord_test.scion_lab_as
+(id, user_email,                   public_ip,   start_port, label,  isd, as_id, status, type,  created, updated)
+VALUES
+(2, 'netsec.test.email@gmail.com', '$INTF_ADDR', 50000,      'AS12', 1,     12,      1,      0, now(),   now());"
+out=$(runSQL "$sql") && stat=0 || stat=$?
+
+sql="INSERT INTO scion_coord_test.attachment_point
+(vpn_ip, start_vpn_ip, end_vpn_ip, as_id)
+SELECT '10.0.8.1', '10.0.8.2',  '10.0.8.254', id
+FROM scion_coord_test.scion_lab_as WHERE user_email='netsec.test.email@gmail.com';"
+out=$(runSQL "$sql") && stat=0 || stat=$?
+
+
+
+
+
+# remove already generated configuration TGZs :
+# rm -rf "$CONFDIR/netsec.test.email*"
+
+# run SCION Coordinator
+./scion-coord &
+scionCoordPid=$!
+
+
+
+# wait until the HTTP service is up, or 5 seconds
+timeout 5 bash -c "until curl --output /dev/null --silent --head --fail $SCION_COORD_URL; do
+    echo 'Waiting for SCION Coord. Service to be up ...'
+    sleep 1
+done"
+
+# if [ "$doTest" -ne 1 ]; then
+#     # only run the coordinator and wait until it finishes
+#     echo "The Coordinator is running with PID: $scionCoordPid"
+#     wait $scionCoordPid
+#     exit $?
+# fi
+
+# TEST SCION COORDINATOR. The requests don't need to have all these headers, but hey were just copied from Chrome for convenience
+echo "Querying SCION Coordinator Service to create an AS, configure it and download its gen folder definition..."
+rm -f cookies.txt
+curl "$SCION_COORD_URL" -I -c cookies.txt -s >/dev/null
+curl "$SCION_COORD_URL/api/login" -H 'Content-Type: application/json;charset=UTF-8' -b cookies.txt --data-binary '{"email":"netsec.test.email@gmail.com","password":"scionscion"}' --compressed -s >/dev/null
+curl "$SCION_COORD_URL/api/as/generateAS" -X POST -H 'Content-Length: 0' -b cookies.txt -s >/dev/null
+curl "$SCION_COORD_URL/api/as/configureAS" -H 'Content-Type: application/json;charset=UTF-8' -b cookies.txt --data-binary '{"asID":1001,"userEmail":"netsec.test.email@gmail.com","isVPN":false,"ip":"127.0.0.210","serverIA":"1-12","label":"Label for AS1001","type":2,"port":50050}' -s >/dev/null
+GENFOLDERTMP=$(mktemp -d)
+rm -rf "$GENFOLDERTMP"
+mkdir -p "$GENFOLDERTMP"
+curl "$SCION_COORD_URL/api/as/downloadTarball/1001" -b cookies.txt --output "$GENFOLDERTMP/1001.tgz" -s >/dev/null
+rm -f cookies.txt
+
+if [ ! -f "$GENFOLDERTMP/1001.tgz" ]; then
+    exitMessage="Cannot find the (presumably) downloaded file $GENFOLDERTMP/1001.tgz\nFAIL"
+    exit 101
+fi
+cd "$GENFOLDERTMP"
+tar xf "1001.tgz"
+if [ ! -d "netsec.test.email@gmail.com_1-1001/gen/ISD1/AS1001" ]; then
+    exitMessage="Unknown TGZ structure. Cannot continue\nABORT"
+    exit 1
+fi
+
+# safety check:
+rm -rf "$SC/gen/ISD1/AS1001"
+# if [ -d "$SC/gen/ISD1/AS1001" ]; then
+#     pushd "$SC/gen/ISD1" >/dev/null
+#     mv "AS1001" "AS1001-renamed-testing-scion-coord-$(date +%Y.%m.%dT%H:%M:%S.%N)"
+#     popd >/dev/null
+# fi
+cp -r "netsec.test.email@gmail.com_1-1001/gen/ISD1/AS1001" "$SC/gen/ISD1/"
+rm -rf "$GENFOLDERTMP"
+cd "$SC"
+
+
+# update existing AS12 using the scion box update-gen script
+pushd "$CURRENTWD" >/dev/null
+# run update gen:
+cd "${SCIONBOXLOCATION:?}"
+if [ $(ls sub/util | wc -l) == 0 ]; then
+    git submodule init
+    git submodule update
+fi
+torun="./updateGen.sh"
+params="--url $SCION_COORD_URL --address $INTF_ADDR --accountId $ACC_ID --secret $ACC_PW --updateAS 1-12"
+echo "Calling: $torun $params"
+# sleep 9999999999
+"$torun" "$params"
+popd >/dev/null
+
+# we are done using SCION Coord; shut it down
+kill $scionCoordPid
+scionCoordPid=''
+echo "SCION Coordinator service was stopped."
+
+echo "Running SCION now:"
+cd "$SC"
+"./scion.sh" stop || true
+"./scion.sh" run
+
+echo "Checking logs for successful arrival of beacons to the new AS (or $TESTTIMEOUT seconds)..."
+FOUND=false
+
+exec 4>&2
+exec 2>/dev/null
+# run timeout tail in a subshell because we need the while read loop in this one, to set FOUND to true iff found
+exec 3< <(timeout $TESTTIMEOUT tail -n0 -f "$SC/logs/bs1-1001-1.DEBUG")
+exec 2>&4 4>&-
+SSPID=$!
+while read -u 3 LINE; do
+    if echo $LINE | grep 'Successfully verified PCB' &> /dev/null; then
+        FOUND=true
+        pkill -P $SSPID "timeout" &> /dev/null || true
+    fi
+done
+
+if [[ "$FOUND" = true ]]; then
+    exitMessage="SUCCESS"
+    exit 0
+else
+    exitMessage="FAIL"
+    exit 100
+fi
