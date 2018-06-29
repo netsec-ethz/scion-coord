b4210b268af248e2022b136975e7da5591433f41
Juan A. Garcia Pardo <juagargi@gmail.com>
2018-06-29 20:40:05 +0200

    Make long update script a bit safer

--- scripts/remap_as_identity.sh
+++ scripts/remap_as_identity.sh
@@ -9,13 +9,13 @@ if [ ! -f "$basedir/remap_as_identity.py" ]; then
     wget https://raw.githubusercontent.com/netsec-ethz/scion-coord/master/scripts/remap_as_identity.py -O "$basedir/remap_as_identity.py"
 fi
 cd "$SC"
-./scion.sh stop
-~/.local/bin/supervisorctl -c supervisor/supervisord.conf shutdown
-./tools/zkcleanslate 
+./scion.sh stop || true
+~/.local/bin/supervisorctl -c supervisor/supervisord.conf shutdown || true
+./tools/zkcleanslate  || true
 ./scion.sh clean || true
 
 python3 $basedir/remap_as_identity.py --ia "$IA"
 
-rm ./gen-cache/*
+rm ./gen-cache/* || true
 bash -c 'yes | GO_INSTALL=true ./env/deps' || echo "ERROR: Dependencies failed. Starting SCION might fail!"
 ./scion.sh start
