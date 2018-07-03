df41d6613ea91529d383ddc329e6e6cf475a2b74
Juan A. Garcia Pardo <juagargi@gmail.com>
2018-07-03 11:51:36 +0200

    Fix Vm upgrade script

--- scion_upgrade_script.sh
+++ scion_upgrade_script.sh
@@ -77,20 +77,6 @@ echo "Invoking update script with $ACCOUNT_ID $ACCOUNT_SECRET $IA"
 # systemd files upgrade:
 check_system_files
 
-if ! is_id_standardized "$IA" ; then
-    echo "-----------------------------------------------------------------------------------"
-    echo "We need to map the addresses to the standard"
-    cd "/tmp"
-    wget https://raw.githubusercontent.com/netsec-ethz/scion-coord/master/scripts/remap_as_identity.sh -O remap_as_identity.sh  && doremap=1 || doremap=0
-    if [ "$doremap" == 1 ]; then
-        bash remap_as_identity.sh
-    else
-        echo "Not yet mapping IA IDs"
-    fi
-else
-    echo "SCION IA follows standard."
-fi
-
 UPDATE_BRANCH=$(curl --fail "${SCION_COORD_URL}/api/as/queryUpdateBranch/${ACCOUNT_ID}/${ACCOUNT_SECRET}?IA=${IA}" || true)
 
 if [  -z "$UPDATE_BRANCH"  ]
@@ -111,7 +97,7 @@ then
     git config --global url.https://github.com/.insteadOf git@github.com:
 fi
 git fetch "$REMOTE_REPO" "$UPDATE_BRANCH"
-rebase_result=$(git rebase "${REMOTE_REPO}/${UPDATE_BRANCH}")
+rebase_result=$(git rebase -Xours "${REMOTE_REPO}/${UPDATE_BRANCH}")
 
 if [[ $rebase_result == *"is up to date"* ]]
 then
@@ -119,22 +105,36 @@ then
 else
     echo "SCION code has been upgraded, stopping..."
 
-    ./scion.sh stop
-    ~/.local/bin/supervisorctl -c supervisor/supervisord.conf shutdown
-    ./tools/zkcleanslate
+    ./scion.sh stop || true
+    ~/.local/bin/supervisorctl -c supervisor/supervisord.conf shutdown || true
+    ./tools/zkcleanslate || true
 
     echo "Reinstalling dependencies..."
     ./scion.sh clean || true
-    mv go/vendor/vendor.json /tmp && rm -r go/vendor/* && mv /tmp/vendor.json go/vendor/
+    mv go/vendor/vendor.json /tmp && rm -r go/vendor && mkdir go/vendor || true
+    mv /tmp/vendor.json go/vendor/ || true
     pushd go >/dev/null
-    govendor sync
+    govendor sync || true
     popd >/dev/null
     bash -c 'yes | GO_INSTALL=true ./env/deps' || echo "ERROR: Dependencies failed. Starting SCION might fail!"
 
     echo "Starting SCION again..."
-    ./scion.sh start
+    ./scion.sh start || true
 fi
-
 RESULT=$(curl -X POST "${SCION_COORD_URL}/api/as/confirmUpdate/${ACCOUNT_ID}/${ACCOUNT_SECRET}?IA=${IA}") || true
 echo "Done, got response from server: ${RESULT}"
+
+if ! is_id_standardized "$IA" ; then
+    echo "-----------------------------------------------------------------------------------"
+    echo "We need to map the addresses to the standard"
+    cd "/tmp"
+    wget https://raw.githubusercontent.com/netsec-ethz/scion-coord/master/scripts/remap_as_identity.sh -O remap_as_identity.sh  && doremap=1 || doremap=0
+    if [ "$doremap" == 1 ]; then
+        bash remap_as_identity.sh || true
+    else
+        echo "Not yet mapping IA IDs"
+    fi
+else
+    echo "SCION IA follows standard."
+fi
 echo "Done."
