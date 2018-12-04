bc50d74e9e1111bce3932d81aced9ff3397a0823
Juan A. Garcia Pardo <juagargi@gmail.com>
2018-12-04 17:38:04 +0100

    Support detach AS

--- scripts/check_as_config.sh
+++ scripts/check_as_config.sh
@@ -3,9 +3,7 @@
 set -e
 
 SCION_COORD_URL="https://www.scionlab.org"
-# SCION_COORD_URL="http://localhost:8080"
 
-BASEDIR=$(dirname $(realpath $0))
 
 if [ ! -f $SC/gen/ia ] || [ ! -f $SC/gen/account_id ] || [ ! -f $SC/gen/account_secret ]; then
     echo "A required file is missing!"
@@ -13,7 +11,6 @@ if [ ! -f $SC/gen/ia ] || [ ! -f $SC/gen/account_id ] || [ ! -f $SC/gen/account_
     echo "Redownload your SCIONLab AS configuration and redeploy otherwise."
     exit 1
 fi
-
 IA=$(cat $SC/gen/ia | sed 's/_/\:/g')
 ACC_ID=$(cat $SC/gen/account_id)
 ACC_PW=$(cat $SC/gen/account_secret)
@@ -25,38 +22,52 @@ HTTP_CODE=$(curl -s -w "%{http_code}" "$SCION_COORD_URL/api/as/getASData/$ACC_ID
 if [ $HTTP_CODE -eq 304 ]; then
     echo "Existing AS configuration is up to date"
     exit 0
-fi
-if [ $HTTP_CODE -ne 200 ]; then
+elif [ $HTTP_CODE -eq 205 ]; then
+    echo "AS is detached"
+elif [ $HTTP_CODE -eq 200 ]; then
+    echo "New AS configuration"
+else
     echo "Unhandled status code received from Coordinator: $HTTP_CODE"
     [ -f /tmp/gen-data.tgz ] && file /tmp/gen-data.tgz | grep ASCII >/dev/null 2>&1 &&  echo "Coordinator message is:" && cat /tmp/gen-data.tgz
     exit 1
 fi
-# we received a new AS configuration
+# we received a new AS configuration or AS was detached
+cd "$SC"
 ./scion.sh stop || true
 
 TMP=$(mktemp -d)
 cd $TMP
-echo "Expanding tar file in $TMP"
-tar xf /tmp/gen-data.tgz
-# copy gen folder:
-DIR=$(ls)
-if [ $(echo $DIR | wc -l) -ne 1 ]; then
-    # failed assertion
-    echo "Expected exactly one entry in the downloaded tar file /tmp/gen-data.tgz but found a different number. Aborting"
-    exit 1
+if [ $HTTP_CODE -eq 200 ]; then
+    echo "Expanding tar file in $TMP"
+    tar xf /tmp/gen-data.tgz
+    # copy gen folder:
+    DIR=$(ls)
+    if [ $(echo $DIR | wc -l) -ne 1 ]; then
+        # failed assertion
+        echo "Expected exactly one entry in the downloaded tar file /tmp/gen-data.tgz but found a different number. Aborting"
+        exit 1
+    fi
+    DIR=$(realpath $TMP/$DIR)
+else # detaching AS
+    cp -r "$SC/gen" .
+    DIR="$TMP"
+    rm -rf $DIR/gen/ISD* $DIR/gen/dispatcher
+    echo "Guessing the new AS configuration version to be $((LOCAL_VER + 1))"
+    [ -f "$DIR/gen/coord_conf.ver" ] && echo $((LOCAL_VER + 1)) > "$DIR/gen/coord_conf.ver"
 fi
-DIR=$(realpath $TMP/$DIR)
 cd $SC
+# backup_gen
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 if [ -d gen ]; then
     mv gen "gen.bak-$TIMESTAMP" || { echo "Failed to rename gen folder. Aborting."; exit 1; }
 fi
 mv $DIR/gen .
 echo "gen folder moved into place"
+
 # copy VPN client file
-if [ -f /etc/openvpn/client.conf ] && [ -f $DIR/client.conf ]; then
+if [[ -f /etc/openvpn/client.conf  && (-f $DIR/client.conf || $HTTP_CODE -eq 205) ]]; then
     echo "Saving a backup copy of /etc/openvpn/client.conf"
-    sudo cp /etc/openvpn/client.conf "/etc/openvpn/client.conf.bak-$TIMESTAMP"
+    sudo mv /etc/openvpn/client.conf "/etc/openvpn/client.conf.bak-$TIMESTAMP"
     sudo systemctl stop "openvpn@client.service" && sleep 2 || true
 else
     echo "No /etc/openvpn/client.conf file found or not using VPN in this AS configuration. Step skipped."
@@ -72,5 +83,5 @@ fi
 echo "Reloading AS configuration"
 ./supervisor/supervisor.sh reload
 ./tools/zkcleanslate || true
-rm ./gen-cache/* || true
+rm -f ./gen-cache/*
 ./scion.sh start nobuild
