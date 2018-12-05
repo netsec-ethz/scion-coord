393b0180bf942aad4e477fb0d6e534ac2a6dbcb6
Juan A. Garcia Pardo <juagargi@gmail.com>
2018-12-05 10:23:33 +0100

    Filter what to return.
    Only return ASes that are active, created or updated, with connections
    to the AP that are also in those states.

--- controllers/api/scion_lab_ap.go
+++ controllers/api/scion_lab_ap.go
@@ -510,17 +510,26 @@ func (s *SCIONLabASController) GetConnectionsForAP(w http.ResponseWriter, r *htt
 	}
 	// var conns []APConnectionInfo
 	conns := []APConnectionInfo{}
-	for _, cn := range cns {
-		if cn.RespondStatus != models.Active &&
-			cn.RespondStatus != models.Create &&
-			cn.RespondStatus != models.Update {
-			// not pending to add or already active -> don't send info
-			continue
+	shouldStatusBeInAP := func(status uint8) bool {
+		switch status {
+		case models.Active:
+			return true
+		case models.Create:
+			return true
+		case models.Update:
+			return true
+		default:
+			return false
 		}
+	}
+	for _, cn := range cns {
 		if cn.Updated.Unix() > cutoff {
 			continue
 		}
 		userAS := cn.GetJoinAS()
+		if !shouldStatusBeInAP(cn.RespondStatus) || !shouldStatusBeInAP(userAS.Status) {
+			continue
+		}
 		cnInfo := APConnectionInfo{
 			ASID:      userAS.IAString(),
 			IsVPN:     cn.IsVPN,
