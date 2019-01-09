7ed8c598142f51b29c1d3070a77be98bd7449460
Juan A. Garcia Pardo <juagargi@gmail.com>
2019-01-09 18:54:01 +0100

    Changes from review.
    Small fixes plus also returning the version in a header when
    replying to remove the local AS.

--- scripts/check_as_config.sh
+++ scripts/check_as_config.sh
@@ -39,6 +39,21 @@ while getopts "hf" opt; do
 done
 
 
+onexit() {
+    CODE=$?
+    if [ $CODE -ne 0 ]; then
+        exit $CODE
+    fi
+    [ ! -z "$TEMPTGZ" ] && rm -f "/tmp/${TEMPTGZ}"
+    [ ! -z "$TEMPHEADERS" ] && rm -f "/tmp/${TEMPHEADERS}"
+    [ ! -z "$TMP" ] && rm -rf "$TMP"
+}
+trap onexit EXIT
+
+if [ -f "$SC/gen/coord_url" ]; then
+    SCION_COORD_URL=$(cat "$SC/gen/coord_url")
+    echo "Special Coordinator in use. URL: $SCION_COORD_URL"
+fi
 if [ ! -f $SC/gen/ia ] || [ ! -f $SC/gen/account_id ] || [ ! -f $SC/gen/account_secret ]; then
     echo "A required file is missing!"
     echo "Check that the directory \"$SC\" contains: \"ia\", \"account_id\" and \"account_secret\". "
@@ -52,8 +67,11 @@ ACC_PW=$(cat $SC/gen/account_secret)
 [ $force -eq 1 ] && LOCAL_VER="force=1" || LOCAL_VER="local_version=$LOCAL_VER"
 
 cd /tmp
-rm -f gen-data.tgz
-HTTP_CODE=$(curl -s -w "%{http_code}" "$SCION_COORD_URL/api/as/getASData/$ACC_ID/$ACC_PW/$IA?local_version=${LOCAL_VER}${FORCEFLAG}" --output gen-data.tgz)
+TEMPTGZ=$(mktemp gen-data-XXXXXX.tgz)
+TEMPHEADERS=$(mktemp headers-XXXXXX.txt)
+rm -f $TEMPTGZ $TEMPHEADERS
+HTTP_CODE=$(curl -s -w "%{http_code}" "$SCION_COORD_URL/api/as/getASData/$ACC_ID/$ACC_PW/$IA?${LOCAL_VER}" --output ${TEMPTGZ} -D ${TEMPHEADERS}) || { echo "curl failed with code $?. Aborting"; exit 1; }
+VERSION_IN_COORD=$(grep -oP 'Coord_conf\.ver: \K\w+' $TEMPHEADERS || true)
 if [ $HTTP_CODE -eq 304 ]; then
     echo "Existing AS configuration is up to date"
     exit 0
@@ -63,7 +81,7 @@ elif [ $HTTP_CODE -eq 200 ]; then
     echo "New AS configuration"
 else
     echo "Unhandled status code received from Coordinator: $HTTP_CODE"
-    [ -f /tmp/gen-data.tgz ] && file /tmp/gen-data.tgz | grep ASCII >/dev/null 2>&1 &&  echo "Coordinator message is:" && cat /tmp/gen-data.tgz
+    [ -f /tmp/${TEMPTGZ} ] && file /tmp/${TEMPTGZ} | grep ASCII >/dev/null 2>&1 &&  echo "Coordinator message is:" && cat /tmp/${TEMPTGZ}
     exit 1
 fi
 # we received a new AS configuration or AS was detached
@@ -74,21 +92,21 @@ TMP=$(mktemp -d)
 cd $TMP
 if [ $HTTP_CODE -eq 200 ]; then
     echo "Expanding tar file in $TMP"
-    tar xf /tmp/gen-data.tgz
+    tar xf /tmp/${TEMPTGZ}
     # copy gen folder:
     DIR=$(ls)
     if [ $(echo $DIR | wc -l) -ne 1 ]; then
         # failed assertion
-        echo "Expected exactly one entry in the downloaded tar file /tmp/gen-data.tgz but found a different number. Aborting"
+        echo "Expected exactly one entry in the downloaded tar file /tmp/${TEMPTGZ} but found a different number. Aborting"
         exit 1
     fi
     DIR=$(realpath $TMP/$DIR)
-else # detaching AS
-    cp -r "$SC/gen" .
+else # must be code 205: detaching AS
+    cp -rL "$SC/gen" .
     DIR="$TMP"
     rm -rf $DIR/gen/ISD* $DIR/gen/dispatcher
     echo "Guessing the new AS configuration version to be $((LOCAL_VER + 1))"
-    [ -f "$DIR/gen/coord_conf.ver" ] && echo $((LOCAL_VER + 1)) > "$DIR/gen/coord_conf.ver"
+    echo "$VERSION_IN_COORD" > "$DIR/gen/coord_conf.ver"
 fi
 cd $SC
 # backup_gen
@@ -103,7 +121,7 @@ echo "gen folder moved into place"
 if [[ -f /etc/openvpn/client.conf  && (-f $DIR/client.conf || $HTTP_CODE -eq 205) ]]; then
     echo "Saving a backup copy of /etc/openvpn/client.conf"
     sudo mv /etc/openvpn/client.conf "/etc/openvpn/client.conf.bak-$TIMESTAMP"
-    sudo systemctl stop "openvpn@client.service" && sleep 2 || true
+    sudo systemctl stop "openvpn@client.service" || true
 else
     echo "No /etc/openvpn/client.conf file found or not using VPN in this AS configuration. Step skipped."
 fi
